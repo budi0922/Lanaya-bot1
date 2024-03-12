9f1fcf6a1347fa6ac96167aa0c186808c7557349
budi0922 <60764560+budi0922@users.noreply.github.com>
2024-03-12 09:49:02 +0700

    Update owner-dan-menu.js

--- owner-dan-menu.js
+++ owner-dan-menu.js
@@ -1,20 +1,50 @@
 const fs = require('fs')
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
+global.namabot = "Bot Budianto Store"
+global.namaowner = "Budianto"
 global.footer_text = "© " + namabot
-global.pp_bot = fs.readFileSync("./image/foto.jpg")
-global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['628577776666','628577775555']
+global.pp_bot = fs.readFileSync("./image/lol1.jpg")
+global.qris = fs.readFileSync("./image/donasi1.jpg")
+global.owner = ['6282216565720','6285794178917']
 global.pairingNumber = ""
 global.sessionName = 'session'
 global.prefa = ['-_-']
-global.caption_pay = `Qris All Pay
-Ovo
-Dana
-Gopay
+global.caption_pay = `💳 Payment :
+*QRIS ALL PAYMENT* 
+- Bayar via qris +1% pajak (HARGA +1%)
+- contoh 10.000 + 1% = 10.100
 
-Mau ganti payment? ketik .setpay
+*E-WALET*
+- 082216565720 (Ewallet : DANA, OVO, GOPAY, SHOPEEPAY)
+A/N BUDIANTO 
+Dana +100
+Tf dari bank ke dana +500 | bank ke ovo +1500
+
+*BANK*
+BI-FAST : 082216565720
+- BCA 
+Norek : 2990702619 
+A/N BUDIANTO
+
+- OCBC NISP
+Norek : 693815440419 
+A/N BUDIANTO
+
+- BRI 
+Norek : 429701039085535
+A/N BUDIANTO
+
+- Mandiri
+Norek : 1340024414343
+A/N BUDIANTO
+
+- SEABANK 
+901710113223 
+A/N BUDIANTO
+
+- KEB HANA 
+Norek : 12271073170
+A/N BUDIANTO
 `
 //menu bot rapihin sendiri ya, belajar lah jadi anak mandiri.
 module.exports.helpMenu = (pushname) =>{
